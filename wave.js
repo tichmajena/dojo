53d5b4831601ecc7140426fd07ed17359a43a5a9
tmajena <tichmajena@gmail.com>
2021-01-14 21:39:41 +0200

    wavesurfer implementation

---
+++ wave.js
@@ -0,0 +1,77 @@
+const btn = {
+  play: document.getElementById("btn-play"),
+  pause: document.getElementById("btn-pause"),
+  stop: document.getElementById("btn-stop"),
+};
+
+let Spectrum = WaveSurfer.create({
+  container: "#audio-spectrum",
+  progressColor: "#03a9f4",
+});
+
+// Play Button
+btn.play.addEventListener(
+  "click",
+  () => {
+    Spectrum.play();
+
+    btn.stop.disabled = false;
+    btn.pause.disabled = false;
+    btn.play.disabled = true;
+  },
+  false
+);
+
+// Pause Button
+btn.pause.addEventListener(
+  "click",
+  () => {
+    Spectrum.pause();
+
+    btn.stop.disabled = false;
+    btn.pause.disabled = true;
+    btn.play.disabled = false;
+  },
+  false
+);
+
+// Stop Button
+btn.stop.addEventListener(
+  "click",
+  () => {
+    Spectrum.stop();
+
+    btn.stop.disabled = true;
+    btn.pause.disabled = false;
+    btn.play.disabled = false;
+  },
+  false
+);
+
+Spectrum.on("ready", () => {
+  btn.play.disabled = false;
+});
+
+window.addEventListener(
+  "resize",
+  () => {
+    //Get the current progress
+    let currentProgress = Spectrum.getCurrentTime() / Spectrum.getDuration();
+
+    // Reset graph
+    Spectrum.empty();
+    Spectrum.drawBuffer();
+
+    //Set Original Position
+    Spectrum.seekTo(currentProgress);
+
+    // Enable/Disable buttons respectively
+    btn.stop.disabled = false;
+    btn.pause.disabled = true;
+    btn.play.disabled = false;
+  },
+  false
+);
+
+//load the audio
+Spectrum.load("assets/audio/kufun-dojo.mp3");
